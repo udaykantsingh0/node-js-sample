69b96c5cc7d0db365ef17d772e97ea1068238381
“usernamessucks” <sampratap22@outlook.com>
2024-02-12 15:03:06 +0530

    changing

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('rockstar, ,dancing          aadiman v baba yaga !')
+  response.send('Hello World !')
 })
 
 app.listen(app.get('port'), function() {
