dfe380053d07b2635a8208d7a268c74b4d6ca305
“usernamessucks” <sampratap22@outlook.com>
2024-02-16 00:53:47 +0530

    agent

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World dev2!')
+  response.send('this is dev2')
 })
 
 app.listen(app.get('port'), function() {
