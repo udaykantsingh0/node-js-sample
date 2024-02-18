4c4c423f23094fa9b6585b9a57b731771a8b508c
“usernamessucks” <sampratap22@outlook.com>
2024-02-19 00:40:38 +0530

    nginx.conf

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('This is  Docker Ngnix !')
+  response.send('Hello World ; This is  Docker Ngnix !')
 })
 
 app.listen(app.get('port'), function() {
