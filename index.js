3886c0df8a6ba902f9bdcade485f858202cebdc8
“usernamessucks” <sampratap22@outlook.com>
2024-02-20 21:25:05 +0530

    changes in index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var express = require('express')
 var app = express()
 
-app.set('port', (process.env.PORT || 5000))
+app.set('port', (process.env.PORT || 3000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World ');
-  response.send('This is main Branch');
+  response.send('Hello World ')
+  response.send('This is main Branch')
 })
 
 app.listen(app.get('port'), function() {
