a5051ede048d9fca5b0287e05fab5e91409273ce
“usernamessucks” <sampratap22@outlook.com>
2024-03-22 11:41:11 +0530

    aks

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 3000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('This is aws-devops')
+  response.send('Welcome to AKS !')
 })
 
 app.listen(app.get('port'), function() {
