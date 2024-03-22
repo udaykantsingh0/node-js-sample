fe9aebf4258cf2bc74fcc581503f43bb46cab295
“usernamessucks” <sampratap22@outlook.com>
2024-03-22 16:26:05 +0530

    aks

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 3000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('hello world !')
+  response.send('hello AKS !')
 })
 
 app.listen(app.get('port'), function() {
